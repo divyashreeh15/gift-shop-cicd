5d61c1d93bb5d909c64d82dbba618339d068bf7f
divyashreeh15 <shreedivya1920@gmail.com>
2025-08-09 07:15:31 +0530

    Sample 2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -2,24 +2,24 @@
 
 -- Dummy data for User Table
 -- Default password: 123456
-INSERT INTO user_table (user_id, password, username, role) VALUES (100001,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'gagan', 'User');
+INSERT INTO user_table (user_id, password, username, role) VALUES (100001,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'Divya', 'User');
 INSERT INTO user_table (user_id, password, username, role) VALUES (100002,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'admin', 'Admin');
-INSERT INTO user_table (user_id, password, username, role) VALUES (100003,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'rhoda', 'User');
-INSERT INTO user_table (user_id, password, username, role) VALUES (100004,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'olive', 'User');
-INSERT INTO user_table (user_id, password, username, role) VALUES (100005,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'aida', 'User');
-INSERT INTO user_table (user_id, password, username, role) VALUES (100006,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'john', 'User');
+INSERT INTO user_table (user_id, password, username, role) VALUES (100003,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'Bhavya', 'User');
+INSERT INTO user_table (user_id, password, username, role) VALUES (100004,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'Havya', 'User');
+INSERT INTO user_table (user_id, password, username, role) VALUES (100005,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'Sham', 'User');
+INSERT INTO user_table (user_id, password, username, role) VALUES (100006,'$2a$10$B4FvjVc2oXqDTFdGGZjT5urzHS6TSdzrQ7p/vFi1rUuZgjXR4.m5K', 'Lalith', 'User');
 -- MYSQL sequence
 -- UPDATE user_id_sequence SET next_val = 100021;
 -- H2 sequence
 ALTER sequence user_id_sequence restart with 100007;
 
 -- Dummy data for Address
-INSERT INTO address (address_id, area, city, pincode, state) VALUES (100001,'Union Street','Mumbai','400001','Maharashtra');
-INSERT INTO address (address_id, area, city, pincode, state) VALUES (100002,'Stonepot Road','Pune','410038','Maharashtra');
+INSERT INTO address (address_id, area, city, pincode, state) VALUES (100001,'Union Street','Mumbai','400001','Karnataka');
+INSERT INTO address (address_id, area, city, pincode, state) VALUES (100002,'Stonepot Road','Pune','410038','Karnataka');
 INSERT INTO address (address_id, area, city, pincode, state) VALUES (100003,'Adams Drive','Banglore','530068','Karnataka');
-INSERT INTO address (address_id, area, city, pincode, state) VALUES (100004,'Illinois Avenue','Mumbai','400072','Maharashtra');
-INSERT INTO address (address_id, area, city, pincode, state) VALUES (100005,'Hog Camp Road','Mumbai','400002','Maharashtra');
-INSERT INTO address (address_id, area, city, pincode, state) VALUES (100006,'West Fork Drive','Pune','411001','Maharashtra');
+INSERT INTO address (address_id, area, city, pincode, state) VALUES (100004,'Illinois Avenue','Mumbai','400072','Karnataka');
+INSERT INTO address (address_id, area, city, pincode, state) VALUES (100005,'Hog Camp Road','Mumbai','400002','Karnataka');
+INSERT INTO address (address_id, area, city, pincode, state) VALUES (100006,'West Fork Drive','Pune','411001','Karnataka');
 
 -- Dummy data for User detail
 INSERT INTO user_details (user_details_id, first_name, last_name, email_id, phone_no, security_question, security_answer, address_id) VALUES (100001, 'Gagandeep', 'Singh', 'singh.gagandeep3911@gmail.com', '8419969059', 'What is the default answer?', 'answer', 100001);
